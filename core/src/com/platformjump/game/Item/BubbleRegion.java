1051e95c7d2e1980fe9955ecd5ca9bb3736d5007
guyueliang <1582086036@qq.com>
2020-09-24 17:17:30 +0800

    Add collision for BubbleRegion object.

--- core/src/com/platformjump/game/Item/BubbleRegion.java
+++ core/src/com/platformjump/game/Item/BubbleRegion.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.ParticleEffect;
+import com.dongbat.jbump.Item;
+import com.platformjump.game.BaseFramework.BaseActor;
 import com.platformjump.game.BaseFramework.BaseGameActor;
 import com.platformjump.game.BaseFramework.BaseGameStage;
 
@@ -15,8 +17,16 @@ public class BubbleRegion extends BaseGameActor {
     public BubbleRegion(float x, float y, BaseGameStage s) {
         super(x, y, s);
         Off = loadTexture("Traps/Fan/Off.png");
-        On = loadAnimationFromSheet("Traps/Fan/On.png",1,4,0.1f,false);
-        setAnimation(Off);
+        On = loadAnimationFromSheet("Traps/Fan/On.png",1,4,0.1f,true);
+        setAnimation(On);
+
+        //包围盒的起始坐标与Player的坐标x和y方向的距离
+        bboxX = 0;
+        bboxY = 0;
+        //包围盒的宽度和高度
+        bboxWidth = 28;
+        bboxHeight = 100;
+        item = new Item<BaseActor>(this);
 
         //effect.load(Gdx.files.internal("Particles/bubleNote.p"),Gdx.files.internal("Particles"));
         effect.load(Gdx.files.internal("Particles/ParticleParkRain/ParticleParkRain.p"),Gdx.files.internal("Particles/ParticleParkRain"));
